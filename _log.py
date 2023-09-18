d3d8f0b1e96231d0990a72a2b8a7f1c94970cc0d
LUGIA <79367636+LSZTM@users.noreply.github.com>
2023-09-18 20:54:53 +0530

    Update _log.py

--- _log.py
+++ _log.py
@@ -1,12 +1,32 @@
+import functools
 import logging
-def create_logger():
-    try:
+import inspect
+import traceback
+import sys
+from datetime import datetime
+
+
+
+def log_decorator(fn):
+    logger = logging.getLogger(fn.__module__)
+    line_number = inspect.getsourcelines(fn)[1]
+    module_name = inspect.getmodule(fn).__name__
+
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        now = datetime.now()
+        logger.info(" " * 3 + f"{now} - Going to run the '{fn.__name__}' method (Module: {module_name}, Line: {line_number})")
+
+        try:
+            out = fn(*args, **kwargs)
+            logger.info(" " * 3 + f"{now} - Done successfully running the '{fn.__name__}' method (Module: {module_name}, Line: {line_number})")
+            return out
         
-        with open(a:='cortexoa.log', 'x'):
-            pass  
-        
-        logging.basicConfig(filename=a, level=logging.INFO)
-    
-    except OSError as e:
-        logging.error(f"{e}: occurred while creating logger")
-    return a 
+        except Exception as err:
+            logger.error(f"\nError occurred in method '{fn.__name__}' (Module: {module_name}, Line: {line_number}):")
+            logger.error(f"Error Type: {type(err).__name__}")
+            logger.error(f"Error Message: {str(err)}")
+            logger.error(f"Traceback:\n{traceback.format_exc()}")
+            sys.exit(1)
+
+    return wrapper
